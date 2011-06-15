c156f1aa8001d99f9f2a0f7c557d6445c5b0a1bb
Dmitry Sigaev <dsigaev@ggasoftware.com>
2011-06-15 19:59:53 +0400

    mac memory leaks detection for checking please use Instruments->Allocations check Created & Still Living flag on allocation lifespan tab(продолжительность жизни)

---
+++ cppRMM/polymorphism_issues/pm_mem_issue/pm_mem_issue_mac/pm_mem_issue_mac/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pm_mem_issue_mac
+//
+//  Created by Dmitry Sigaev on 6/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
